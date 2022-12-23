7637f4e4ce50d3fdff36c590aafe6aa3c416332a
sayondeep <sayondeep0710@rediffmail.com>
2022-12-24 00:15:46 +0530

    dev

---
+++ Mosh/Javascript- Basics/1.getting started/index.js
@@ -0,0 +1,3 @@
+let name='Sayon Deep';
+console.log(name);
+
---
+++ Mosh/Javascript- Basics/2.Basics/index2.2.js
@@ -0,0 +1,6 @@
+let interestRate = 0.3;
+//const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+typeof (interestRate)
